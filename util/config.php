55e76892e4afcaef6974e86c0f7466a6cca8b2c0
Starlyyy <izabella.costa.tds2023@gmail.com>
2025-08-15 13:27:58 -0300

    Instalacao no meu notebook

---
+++ util/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//Mostrar erros do PHP
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Configurar essas variáveis de acordo com o seu ambiente
+define("DB_HOST", "localhost");
+define("DB_NAME", "db_alunos");
+define("DB_USER", "root");
+define("DB_PASSWORD", "bancodedados");
+
+//configuracao do ambiente
+define("AMB_DEV", true);
+// define("AMB_DEV", false);
---
+++ view/alunos/inserir.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once(__DIR__ . "/../../model/Aluno.php");
+require_once(__DIR__ . "/../../controller/AlunoController.php");
+
+$msgErro = "";
+
+//Receber os dados do formulário
+if(isset($_POST['nome'])) {
+    //Usuário já clicou no gravar
+    $nome        = trim($_POST['nome']) ? trim($_POST['nome']) : null ;
+    $idade       = is_numeric($_POST['idade']) ? $_POST['idade'] : null ;
+    $estrangeiro = trim($_POST['estrang']) ? trim($_POST['estrang']) : null;
+    $idCurso     = is_numeric($_POST['curso']) ? $_POST['curso'] : null;;
+
+    //Criar um objeto Aluno para persistí-lo
+    $aluno = new Aluno();
+    $aluno->setNome($nome);
+    $aluno->setIdade($idade);
+    $aluno->setEstrangeiro($estrangeiro);
+
+    $curso = new Curso();
+    $curso->setId($idCurso);
+    $aluno->setCurso($curso);
+    //print_r($aluno);
+
+    //Chamar o DAO para salvar o objeto Aluno
+    $alunoCont = new AlunoController();
+    $erros = $alunoCont->inserir($aluno);
+
+    //Redirecionar para o listar
+    if (!$erros) {
+        header("location: listar.php");
+        # code...
+    } else {
+        $msgErro = implode("<br>", $erros);
+        // echo $msgErro;
+    }
+}
+
+
+include_once(__DIR__ . "/form.php");
+?>
